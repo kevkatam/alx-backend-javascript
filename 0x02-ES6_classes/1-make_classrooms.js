470e09077d6779dcb760041f7d21fd36705c2924
kevkatam <kevkatam@gmail.com>
2024-03-07 16:40:20 +0300

    adding 1-make_classrooms.js

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,7 @@
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  const arr = [];
+  arr.push(new ClassRoom(19), new ClassRoom(20), new ClassRoom(34));
+  return arr;
+}
