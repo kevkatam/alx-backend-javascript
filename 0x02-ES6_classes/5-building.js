3d943306d5e73592433cd543d818e48c3d3378bf
kevkatam <kevkatam@gmail.com>
2024-03-07 23:02:40 +0300

    adding 5-building.js

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,21 @@
+export default class Building {
+  constructor(sqft) {
+    this.sqft = sqft;
+    if (
+      new.target !== Building
+      && typeof this.evacuationWarningMessage !== 'function'
+    ) {
+      throw new Error(
+        'Class extending Building must override evacuationWarningMessage'
+      );
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    this._sqft = value;
+  }
+}
