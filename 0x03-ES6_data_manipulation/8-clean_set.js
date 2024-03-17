6f9cc32634c8fa4f8cf69dc7ac071fbd197910bc
kevkatam <kevkatam@gmail.com>
2024-03-17 17:19:01 +0300

    adding 8-clean_set.js

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,13 @@
+export default function cleanSet(set, startString) {
+  if (startString === '') {
+    return '';
+  }
+  const arr = [];
+
+  for (const elem of set) {
+    if (elem.startsWith(startString)) {
+      arr.push(elem.slice(startString.length));
+    }
+  }
+  return arr.join('-');
+}
