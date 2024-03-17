b1fc9f02120d7b6da668c712b871f47577e0a855
kevkatam <kevkatam@gmail.com>
2024-03-17 17:28:57 +0300

    modified 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,11 @@
 export default function cleanSet(set, startString) {
-  if (startString === '') {
+  if (startString === '' || typeof startString !== 'string') {
     return '';
   }
   const arr = [];
 
   for (const elem of set) {
-    if (elem.startsWith(startString)) {
+    if (elem.startsWith(startString) && typeof elem === 'string') {
       arr.push(elem.slice(startString.length));
     }
   }
