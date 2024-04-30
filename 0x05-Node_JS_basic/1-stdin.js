d36ae27dfda54e5e71705597931ec9c18d4f8fa3
kevkatam <kevkatam@gmail.com>
2024-04-30 22:10:21 +0300

    adding 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,5 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.setEncoding('utf8');
 
 if (process.stdin.isTTY) {
   process.stdin.on('data', (data) => {
