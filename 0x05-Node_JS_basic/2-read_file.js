9ab91fb0b1ad537507b4fa963772454529dc32e7
kevkatam <kevkatam@gmail.com>
2024-04-30 23:13:49 +0300

    adding 2-read_file.js

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  const fields = {};
+  const students = {};
+  let length = 0;
+
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+    const lines = data.toString().split('\n');
+
+    lines.forEach((line) => {
+      if (line) {
+        length += 1;
+        const field = line.toString().split(',');
+        if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+          students[field[3]].push(field[0]);
+        } else {
+          students[field[3]] = [field[0]];
+        }
+        if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+          fields[field[3]] += 1;
+        } else {
+          fields[field[3]] = 1;
+        }
+      }
+    });
+    const len = length - 1;
+    console.log(`Number of students: ${len}`);
+    for (const [k, v] of Object.entries(fields)) {
+      if (k !== 'field') {
+        console.log(`Number of students in ${k}: ${v}. List: ${students[k].join(', ')}`);
+      }
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+module.exports = countStudents;
