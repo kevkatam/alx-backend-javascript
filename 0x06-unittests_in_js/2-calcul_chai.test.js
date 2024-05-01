a8af986d44d5a1abef2a12895c0bd2e2ac75e13d
kevkatam <kevkatam@gmail.com>
2024-05-01 21:59:02 +0300

    adding 2-calcul_chai.js, 2-calcul_chai.test.js

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,25 @@
+const chai = require('chai');
+const calculateNumber = require('./1-calcul.js');
+const expect = chai.expect;
+
+describe('calculateNumber', function () {
+  it('should return sum', function () {
+    res = calculateNumber('SUM', 1.4, 4.5);
+    expect(res).to.equal(6);
+  });
+
+  it('should return subtraction', function () {
+    res = calculateNumber('SUBTRACT', 1.4, 4.5);
+    expect(res).to.equal(-4);
+  });
+
+  it('should return division', function () {
+    res = calculateNumber('DIVIDE', 1.4, 4.5);
+    expect(res).to.equal(0.2);
+  });
+
+  it('should return Error', function () {
+    res = calculateNumber('DIVIDE', 1.4, 0);
+    expect(res).to.equal('Error');
+  });
+});
