eefe2fe18cea5c592df8f22edf0c28d86a17d6f3
kevkatam <kevkatam@gmail.com>
2024-05-01 15:54:52 +0300

    modified 0-calcul.test.js

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,15 +3,15 @@ const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', function () {
   it('...', function () {
-    assert.equal(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(1, 3), 4);
   });
   it('...', function () {
-    assert.equal(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
   });
   it('...', function () {
-    assert.equal(calculateNumber(1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
   });
   it('...', function () {
-    assert.equal(calculateNumber(1.5, 3.7), 6);
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
 });
