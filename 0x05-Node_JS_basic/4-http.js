75cff85926cf53732a1a054d5acdaa350b8f6f5e
kevkatam <kevkatam@gmail.com>
2024-05-03 10:47:15 +0300

    adding 4-http.js

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,13 @@
+const { createServer } = require('node:http');
+
+const port = 1245;
+const hostname = '127.0.0.1';
+
+const app = createServer((req, res) => {
+  res.statusCode = 200;
+  res.end('Hello Holberton School!');
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
